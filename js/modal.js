54f90839ab11b32e8b2d55d60bfc7db019f5bf08
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-10-17 15:47:27 +0200

    modal style added

--- js/modal.js
+++ js/modal.js
@@ -9,9 +9,14 @@ var getParent = (el, num = 1) => {
     for (i = 0; i < num && el != body; i++) el = el.parentElement;
     return el;
 };
+/**Returns number if positive or 0 else
+ * @param {*} num
+ * @returns number || 0
+ */
+var getPositive = (num) => (num > 0 ? num : 0);
 var modal = function () {
     this.form = document.querySelector(".modal-container.modal-form");
-    this.success = document.querySelector(".modal-container:not(.modal-form)");
+    this.thanks = document.querySelector(".modal-container:not(.modal-form)");
     this.activeRadio = null;
     this.open = (modal, selected = null) => {
         try {
@@ -34,31 +39,30 @@ var modal = function () {
             modal.style.display = "none";
         } catch (err) {}
     };
-    this.resize = () => {
-        this.form.style.height = `${document.body.offsetHeight}px`;
-    };
     this.load = () => {
-        this.resize();
-        //this.close(this.form);
-        this.close(this.success);
+        this.close(this.form);
+        this.close(this.thanks);
     };
     return this;
 }.call({});
 window.addEventListener("load", modal.load);
-window.addEventListener("resize", modal.resize);
 document.querySelector(".close-modal").addEventListener("click", (e) => {
     modal.close(getParent(e.target, 2));
 });
+document
+    .querySelector(".thanks button")
+    .addEventListener("click", () => modal.close(modal.thanks));
 document
     .querySelector(".title .green-button")
     .addEventListener("click", () => modal.open(modal.form));
 document.getElementsByName("radio").forEach((r) => {
     let parent = getParent(r, 2);
-    parent.addEventListener("click", () => {
-        if (modal.activeRadio) {
-            modal.activeRadio.classList.remove("label-active");
-        }
-        parent.classList.add("label-active");
-        modal.activeRadio = parent;
-    });
+    if (!r.disabled)
+        parent.addEventListener("click", () => {
+            if (modal.activeRadio) {
+                modal.activeRadio.classList.remove("label-active");
+            }
+            parent.classList.add("label-active");
+            modal.activeRadio = parent;
+        });
 });
